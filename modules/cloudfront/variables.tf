eb49d662a87fb7b67ee8f8666b60a59fe45e247c
Nicholas Cabral <nicholasscabral@gmail.com>
2025-02-14 22:17:11 -0300

    feat: s3 and cloudfront config

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,11 @@
+variable "origin_id" {
+  type        = string
+  default     = ""
+  description = "Id de origem"
+}
+
+variable "bucket_domain_name" {
+  type        = string
+  default     = ""
+  description = "Dominio do bucket"
+}
